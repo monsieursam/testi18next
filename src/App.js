b2d3b341ead9809e081cc0a6d4c41766bec99d91
Sam <sam.mamane@hotmail.fr>
2019-10-04 14:41:36 +0200

    Stage all change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
+import i18next from 'i18next'
+
 import './App.css';
 
 function App() {
+  i18next.init(
+    {
+      lng: 'en',
+      resources: {
+        en: {
+          translation: {
+            key: 'hello world'
+          }
+        }
+      }
+    },
+    () => {
+      // initialized and ready to go!
+      console.log('key: ')
+    }
+  )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> {i18next.t('key')}
         </p>
         <a
           className="App-link"
